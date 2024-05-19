3498aa55dbbaf6fef888c50fb1466c0f1a19fbb0
Jomarcenter-mjm <jomarlive@gmail.com>
2024-05-19 20:44:35 +0800

    Initial commit

---
+++ Tile01.tsx
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Tile01" tilewidth="256" tileheight="512" tilecount="21" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="512" source="assets/dirt_E.png"/>
+ </tile>
+ <tile id="1">
+  <image width="256" height="512" source="assets/dirt_N.png"/>
+ </tile>
+ <tile id="2">
+  <image width="256" height="512" source="assets/dirtFarmland_E.png"/>
+ </tile>
+ <tile id="3">
+  <image width="256" height="512" source="assets/dirtFarmland_N.png"/>
+ </tile>
+ <tile id="4">
+  <image width="256" height="512" source="assets/dirtFarmland_S.png"/>
+ </tile>
+ <tile id="5">
+  <image width="256" height="512" source="assets/dirtFarmland_W.png"/>
+ </tile>
+ <tile id="6">
+  <image width="256" height="512" source="assets/corn_N.png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="512" source="assets/corn_S.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="512" source="assets/corn_W.png"/>
+ </tile>
+ <tile id="9">
+  <image width="256" height="512" source="assets/cornDouble_E.png"/>
+ </tile>
+ <tile id="10">
+  <image width="256" height="512" source="assets/cornDouble_N.png"/>
+ </tile>
+ <tile id="11">
+  <image width="256" height="512" source="assets/cornDouble_S.png"/>
+ </tile>
+ <tile id="12">
+  <image width="256" height="512" source="assets/cornDouble_W.png"/>
+ </tile>
+ <tile id="13">
+  <image width="256" height="512" source="assets/cornYoung_E.png"/>
+ </tile>
+ <tile id="14">
+  <image width="256" height="512" source="assets/cornYoung_N.png"/>
+ </tile>
+ <tile id="15">
+  <image width="256" height="512" source="assets/cornYoung_S.png"/>
+ </tile>
+ <tile id="16">
+  <image width="256" height="512" source="assets/cornYoung_W.png"/>
+ </tile>
+ <tile id="17">
+  <image width="256" height="512" source="assets/cornYoungDouble_E.png"/>
+ </tile>
+ <tile id="18">
+  <image width="256" height="512" source="assets/cornYoungDouble_N.png"/>
+ </tile>
+ <tile id="19">
+  <image width="256" height="512" source="assets/cornYoungDouble_S.png"/>
+ </tile>
+ <tile id="20">
+  <image width="256" height="512" source="assets/cornYoungDouble_W.png"/>
+ </tile>
+</tileset>
